bb7c42f4413ca801c139da396f5bfb79ccf01307
Jean-André Santoni <jean.andre.santoni@gmail.com>
2020-12-29 22:33:53 +0700

    Try to punch from both sides

--- client.lua
+++ client.lua
@@ -5,45 +5,26 @@ local udp = socket.udp()
 udp:setpeername("127.0.0.1", 1234)
 
 udp:send("hi")
-local data = udp:receive()
+local data = assert(udp:receive())
 local my = json.parse(data)
 print("I am", my.ip, my.port)
 
-udp:send("list")
-local data = udp:receive()
-local map = json.parse(data)
+local data = assert(udp:receive())
+local peer = json.parse(data)
+print("I see", peer.ip, peer.port)
 
-local peer = nil
-for k,p in pairs(map) do
-	if p.ip ~= my.ip or p.port ~= my.port then
-		peer = p
-		break
-	end
-end
 assert(udp:close())
 
-if peer == nil then
-	print("acting as server on", my.ip, my.port)
-	local udp2 = assert(socket.udp())
-	assert(udp2:setsockname('*', my.port))
-	while true do
-		local data, ip, port = assert(udp2:receivefrom())
-		print("received", data, ip, port)
-		peer = {ip=ip, port=port}
-		assert(udp2:sendto("hohai", peer.ip, peer.port))
-	end
-end
-
-if peer ~= nil then
-	print("acting as client for", peer.ip, peer.port)
-	print("sending hello")
-	local udp2 = assert(socket.udp())
-	assert(udp2:setpeername(peer.ip, peer.port))
-	assert(udp2:send("hello"))
-	while true do
-		local data = assert(udp2:receive())
-		print("received", data)
-		assert(udp2:send("hohai"))
-	end
+local udp2 = assert(socket.udp())
+assert(udp2:setsockname('*', my.port))
+print("sending hello")
+assert(udp2:sendto("hello", peer.ip, peer.port))
+os.execute("sleep 1")
+print("sending hello")
+assert(udp2:sendto("hello", peer.ip, peer.port))
+while true do
+	local data = assert(udp2:receive())
+	print("received", data)
+	assert(udp2:sendto("hohai", peer.ip, peer.port))
 end
 
--- server.lua
+++ server.lua
@@ -4,20 +4,22 @@ local json = require("json")
 local udp = socket.udp()
 udp:setsockname('*', 1234)
 
-local map = {}
+local first = nil
+local second = nil
 
 while true do
 	data, ip, port = udp:receivefrom()
 	print(data, ip, port)
 	if data == "hi" then
-		local id = ip..":"..port
-		map[id] = {}
-		map[id].ip = ip
-		map[id].port = port
-		local o = {ip=ip, port=port}
-		udp:sendto(json.stringify(o), ip, port)
-	elseif data == "list" then
-		udp:sendto(json.stringify(map), ip, port)
+		if first == nil then
+			first = {ip=ip, port=port}
+			udp:sendto(json.stringify(first), first.ip, first.port)
+		else
+			second = {ip=ip, port=port}
+			udp:sendto(json.stringify(second), second.ip, second.port)
+			udp:sendto(json.stringify(second), first.ip, first.port)
+			udp:sendto(json.stringify(first), second.ip, second.port)
+		end
     end
 end
 
