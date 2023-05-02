5f5c8306855dd274ca86a2a8ee67fda862a2c991
Jean-André Santoni <jean.andre.santoni@gmail.com>
2023-05-02 19:21:04 +0000

    Remove json in server

--- server.lua
+++ server.lua
@@ -1,27 +1,26 @@
 local socket = require("socket")
-local json = require("json")
 
 local udp = socket.udp()
 udp:setsockname('*', arg[1] or 1234)
 print("Listening on", '*', arg[1] or 1234)
 
-local first = nil
-local second = nil
+local p1 = nil
+local p2 = nil
 
 while true do
 	data, ip, port = udp:receivefrom()
 	print("Received", data, ip, port)
 	if data == "hi" then
-		if first == nil then
-			first = {ip=ip, port=port, idx=1}
-			udp:sendto(json.stringify(first), first.ip, first.port)
+		if p1 == nil then
+			p1 = {ip=ip, port=port, idx=1}
+			udp:sendto(p1.idx .. ":" .. p1.ip .. ":" .. p1.port, p1.ip, p1.port)
 		else
-			second = {ip=ip, port=port, idx=2}
-			udp:sendto(json.stringify(second), second.ip, second.port)
-			udp:sendto(json.stringify(second), first.ip, first.port)
-			udp:sendto(json.stringify(first), second.ip, second.port)
-			first = nil
-			second = nil
+			p2 = {ip=ip, port=port, idx=2}
+			udp:sendto(p2.idx .. ":" .. p2.ip .. ":" .. p2.port, p2.ip, p2.port)
+			udp:sendto(p2.idx .. ":" .. p2.ip .. ":" .. p2.port, p1.ip, p1.port)
+			udp:sendto(p1.idx .. ":" .. p1.ip .. ":" .. p1.port, p2.ip, p2.port)
+			p1 = nil
+			p2 = nil
 		end
     end
 end
