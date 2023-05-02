6f73fa52de39342d79d064d939d9d4a55831e6fb
Jean-André Santoni <jean.andre.santoni@gmail.com>
2023-05-02 21:43:45 +0200

    Remove json completely

--- client.lua
+++ client.lua
@@ -1,30 +1,38 @@
 local socket = require("socket")
-local json = require("json")
+
+-- split on :
+local function split(s)
+	local r = {}
+	for w in s:gmatch("[^:]+") do
+		table.insert(r, w)
+	end
+	return r
+end
 
 local rdv = socket.udp()
 rdv:setpeername(arg[1], arg[2])
 
 rdv:send("hi")
 local data = assert(rdv:receive())
-local my = json.parse(data)
-print("I am", my.ip, my.port)
+local my = split(data)
+print("I am", my[1], my[2], my[3])
 
 local data = assert(rdv:receive())
-local peer = json.parse(data)
-print("I see", peer.ip, peer.port)
+local peer = split(data)
+print("I see player idx", peer[1], peer[2], peer[3])
 
 assert(rdv:close())
 
 local p2p = assert(socket.udp())
-assert(p2p:settimeout(0))
-assert(p2p:setsockname('*', my.port))
+--assert(p2p:settimeout(0))
+assert(p2p:setsockname('*', my[3]))
 
 print("sending hello")
-assert(p2p:sendto("hello", peer.ip, peer.port))
+assert(p2p:sendto("hello", peer[2], peer[3]))
 while true do
 	local data = p2p:receive()
 	if data ~= nil then print("received", data) end
 	if data == "hohai" then break end
-	assert(p2p:sendto("hohai", peer.ip, peer.port))
+	assert(p2p:sendto("hohai", peer[2], peer[3]))
 end
 
